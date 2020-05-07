10b75f19ac30ccf6b550faf99e2abb4423ae917b
Daniel Hess <dan9186@gmail.com>
2020-05-07 14:22:32 -0700

    start returning something to test endpoint

--- handlers.go
+++ handlers.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
 func handleSomeEndpoint(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Here be dragons"))
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+	}
+
+	defer r.Body.Close()
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(body)
 }
