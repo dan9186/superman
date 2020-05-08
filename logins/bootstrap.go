024b4b578d49a8fe1c63c461409b77ab0894b035
Daniel Hess <dan9186@gmail.com>
2020-05-07 18:16:19 -0700

    beefing up bootstrap errors

--- logins/bootstrap.go
+++ logins/bootstrap.go
@@ -2,6 +2,7 @@ package logins
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 // BootstrapLogins takes a database object to bootstrap the necessary tables for
@@ -9,12 +10,12 @@ import (
 func BootstrapLogins(db *sql.DB) error {
 	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS logins (uuid TEXT PRIMARY KEY, username TEXT, timestamp INTEGER, ip_address TEXT)")
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to prepare login table creation: %v", err.Error())
 	}
 
 	_, err = stmt.Exec()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to execute login table creation: %v", err.Error())
 	}
 
 	return nil
