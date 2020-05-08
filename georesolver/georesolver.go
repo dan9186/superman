68e003d870ab6623912da3077c916b097d2ff02d
Daniel Hess <dan9186@gmail.com>
2020-05-07 23:41:10 -0700

    add some docs

--- georesolver/georesolver.go
+++ georesolver/georesolver.go
@@ -7,10 +7,14 @@ import (
 	"github.com/oschwald/geoip2-golang"
 )
 
+// GeoResolver represents a geo locational resolution resource. The normal DB
+// meets this interface so that mocks can be made.
 type GeoResolver interface {
 	City(net.IP) (*geoip2.City, error)
 }
 
+// MockGeoDB is a basic representation of all the data necessary to handle a
+// mock lookup and response to a geo locational db.
 type MockGeoDB struct {
 	ExpectedIP net.IP
 	Latitude   float64
@@ -18,6 +22,7 @@ type MockGeoDB struct {
 	Radius     int
 }
 
+// City meets the GeoResolver interface as a lookup for a City by IP Address
 func (mgdb MockGeoDB) City(ip net.IP) (*geoip2.City, error) {
 	if mgdb.ExpectedIP.String() != ip.String() {
 		return nil, fmt.Errorf("incorrect mock IP requested")
