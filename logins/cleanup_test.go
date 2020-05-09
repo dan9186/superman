77f66ecd8c8ba2e1ecc873b48a8fb98826feac39
Daniel Hess <dan9186@gmail.com>
2020-05-08 18:59:50 -0700

    add test ensuring cleanup only does test data

---
+++ logins/cleanup_test.go
@@ -0,0 +1,25 @@
+package logins
+
+import (
+	"testing"
+
+	"github.com/franela/goblin"
+	. "github.com/onsi/gomega"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+)
+
+func TestCleanup(t *testing.T) {
+	g := goblin.Goblin(t)
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+
+	g.Describe("Cleanup", func() {
+		g.It("should store a login event", func() {
+			mockDB, mockAsserts, _ := sqlmock.New()
+			mockAsserts.ExpectExec(`DELETE FROM logins WHERE username = 'cuketest'`).
+				WillReturnResult(sqlmock.NewResult(0, 1))
+
+			err := Cleanup(mockDB)
+			Expect(err).To(BeNil())
+		})
+	})
+}
