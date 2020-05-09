e2ae4b10443966ab7e29398a8fe3098810efa7d5
Daniel Hess <dan9186@gmail.com>
2020-05-08 19:55:45 -0700

    write out the formula

--- logins/location.go
+++ logins/location.go
@@ -4,11 +4,33 @@ import (
 	"math"
 )
 
+const (
+	earthRadius = 3958.8 // miles
+)
+
 // Location represents a geographical location
 type Location struct {
 	Latitude  float64 `json:"lat"`
 	Longitude float64 `json:"lon"`
 	Radius    int     `json:"radius"`
+}
+
+// Distance calculates the distance from the calling location to the provided
+// location in miles.
+func (l *Location) Distance(l2 *Location) float64 {
+	lat1, lon1 := l.Radians()
+	lat2, lon2 := l2.Radians()
+
+	latD := (lat2 - lat1) / 2
+	latDSqrd := math.Sin(latD) * math.Sin(latD)
+
+	lonD := (lon2 - lon1) / 2
+	lonDSqrd := math.Sin(lonD) * math.Sin(lonD)
+
+	sqrtH := math.Sqrt(latDSqrd + math.Cos(lat1)*math.Cos(lat2)*lonDSqrd)
+	d := 2 * earthRadius * math.Asin(sqrtH)
+
+	return math.Round(d*10000) / 10000
 }
 
 // Radians converts the latitude and longitude of a location to radians. It
