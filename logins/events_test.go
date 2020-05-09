348239e0bb375e0e3fb3c2b689cb12e0175758b1
Daniel Hess <dan9186@gmail.com>
2020-05-08 17:48:57 -0700

    use new geoip mock

--- logins/events_test.go
+++ logins/events_test.go
@@ -75,12 +75,10 @@ func TestConfig(t *testing.T) {
 
 		g.Describe("Geo Location", func() {
 			g.It("should resolve an events location", func() {
-				mock := georesolver.MockGeoDB{
-					ExpectedIP: net.ParseIP("206.81.252.6"),
-					Radius:     20,
-					Latitude:   42.4242,
-					Longitude:  42.4242,
-				}
+				mock := georesolver.NewMock()
+
+				mock.ExpectIP(net.ParseIP("206.81.252.6")).
+					WillReturnLocation(42.4242, 42.4242, 20)
 
 				var e Event
 				json.Unmarshal([]byte(validLoginEvent), &e)
@@ -111,12 +109,16 @@ func TestConfig(t *testing.T) {
 							AddRow("d99df0fd-3a77-4662-910f-9e4f8ecfe25b", "1588930045", "24.242.71.20"),
 					)
 
-				mockGeoDB := georesolver.MockGeoDB{
-					ExpectedIP: net.ParseIP("206.81.252.6"),
-					Radius:     20,
-					Latitude:   42.4242,
-					Longitude:  42.4242,
-				}
+				mockGeoDB := georesolver.NewMock()
+
+				mockGeoDB.ExpectIP(net.ParseIP("206.81.252.6")).
+					WillReturnLocation(42.4242, 42.4242, 20)
+
+				mockGeoDB.ExpectIP(net.ParseIP("91.207.175.104")).
+					WillReturnLocation(32.3242, 32.3242, 20)
+
+				mockGeoDB.ExpectIP(net.ParseIP("24.242.71.20")).
+					WillReturnLocation(22.3242, 22.3242, 20)
 
 				var e Event
 				json.Unmarshal([]byte(validLoginEvent), &e)
