2c22d962f88cf07299ca2e507ff2b5568501e84b
Daniel Hess <dan9186@gmail.com>
2020-05-08 19:39:38 -0700

    rough number estimates of a test for calculating distance

--- logins/location_test.go
+++ logins/location_test.go
@@ -22,5 +22,22 @@ func TestLocation(t *testing.T) {
 			Expect(latRad).To(Equal(0.7914))
 			Expect(lonRad).To(Equal(-2.1407))
 		})
+
+		g.It("should calculate the distance between two locations", func() {
+			// West Linn, OR
+			l1 := &Location{
+				Latitude:  45.345,
+				Longitude: -122.6533,
+			}
+
+			// San Diego Zoo
+			l2 := &Location{
+				Latitude:  32.7352,
+				Longitude: -117.149,
+			}
+
+			d := l1.Distance(l2)
+			Expect(d).To(Equal(918.99))
+		})
 	})
 }
