4bfcf57f37a1138177fa388b2271dd3685af3f22
Daniel Hess <dan9186@gmail.com>
2020-05-11 06:46:06 -0700

    there is no need for this to be public

--- logins/location.go
+++ logins/location.go
@@ -22,8 +22,8 @@ type Location struct {
 // decimal places, all outputs will be rounded to a max of 4 decimal places as
 // well. See https://en.wikipedia.org/wiki/Haversine_formula
 func (l *Location) Distance(l2 *Location) float64 {
-	lat1, lon1 := l.Radians()
-	lat2, lon2 := l2.Radians()
+	lat1, lon1 := l.radians()
+	lat2, lon2 := l2.radians()
 
 	latD := (lat2 - lat1) / 2
 	latDSqrd := math.Sin(latD) * math.Sin(latD)
@@ -40,7 +40,7 @@ func (l *Location) Distance(l2 *Location) float64 {
 // Radians converts the latitude and longitude of a location to radians. It
 // returns the values in the order of latitude as radians, and longitude as
 // radians.
-func (l *Location) Radians() (float64, float64) {
+func (l *Location) radians() (float64, float64) {
 	lat := l.Latitude * math.Pi / 180
 	lat = math.Round(lat*10000) / 10000
 
